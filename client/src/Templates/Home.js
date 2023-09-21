0f5c0bdedb2e87db0e4604b262bb325076551a84
dvholmes <desmondholmes8219@gmail.com>
2023-09-21 15:50:30 -0700

    Home page v1 finished

--- client/src/Templates/Home.js
+++ client/src/Templates/Home.js
@@ -3,6 +3,7 @@ import Header from '../components/header'
 import Footer from '../components/footer'
 
 
+
 import '../css_file/homepage.css'
 
 function HomePage(){
@@ -54,6 +55,7 @@ function HomePage(){
                 <div className = 'mission-image-container'>
                     <img src = "Eco-Friendly-Pest-Control-What-You-Need-to-Know.jpg" alt ='Pest Control'/>
                 </div>
+
             </section>
 
 
@@ -69,7 +71,7 @@ function HomePage(){
                     <div className = 'aboutus-content-item'>
 
                         <div className = 'microbialLabs-container'>
-                            <h1>Microbial Labs</h1>
+                            <h1 className = 'content-item-headers'>Microbial Labs</h1>
 
                             <div className = 'microbial-description'>
                                 <p>In our microbial labs, we specialize in the cultivation of bacteria and fungi in pure cultures, carefully nurtured on selective media. Our process is driven by a commitment to 
@@ -113,7 +115,7 @@ function HomePage(){
                     <div className = 'aboutus-content-item'>
                         <div className = 'aboutus-description'>
 
-                            <h2>About Michael Holmes</h2>
+                            <h2 className = 'content-item-headers'>About Michael Holmes</h2>
                             <div className = 'description-container'>
 
                                 <p>Michael Holmes is a seasoned professional with over 28 years of experience in the field of agriculture and environmental science. 
@@ -131,16 +133,160 @@ function HomePage(){
 
                     </div>
 
-                 
-               
+
+                </div>
+            </section>
+
+            <div className = 'innovations-containers'>
+
+                <div className = 'innovation_header'> <h1>Innovations</h1></div>
+
+                <h1>OUR INOCULANTS ARE COMBINED WITH CONVENTIONAL AG PROGRAMS FOR CROP QUALITY</h1>
+            </div>
+
+
+            <section className = 'advantages-section'>
+
+                    <div className = 'advantages-header'>
+                        <h1>ADVANTAGES = GUARANTEE ANALYSIS AND VIABLE CELLS</h1>
+                    </div>
+
+
+                    <div className = 'advantages-gallery-container'>
+                
+
+                        <div className = 'advantage-image-section' >
+                            <div className = 'ad-image-container'>
+                                <img src ='./advantage-images/Hyper-Galaxy-for-suc.png' alt = 'hypergalaxy'/>
+
+                            </div>
+
+                            <div className = 'ad-image-label'>
+                                <h2>REFORESTATION AFTER WILDFIRES </h2>
+                            </div>
+
+                        </div>
+                        <div className = 'advantage-image-section' >
+                            <div className = 'ad-image-container'>
+                                <img src ='./advantage-images/Slide11-JPG.png' alt = 'hypergalaxy'/>
+
+                            </div>
+
+                            <div className = 'ad-image-label'>
+                                <h2>PURE CULTURES</h2>
+                            </div>
+
+                        </div>
+                        <div className = 'advantage-image-section' >
+                            <div className = 'ad-image-container'>
+                                <img src ='./advantage-images/web4-jpg.png' alt = 'hypergalaxy'/>
+
+                            </div>
+
+                            <div className = 'ad-image-label'>
+                                <h2>IMPROVE VAM ROOTS</h2>
+                            </div>
+
+                        </div>
+                        <div className = 'advantage-image-section' >
+                            <div className = 'ad-image-container'>
+                                <img src ='./advantage-images/web6-jpg.png' alt = 'hypergalaxy'/>
+
+                            </div>
+
+                            <div className = 'ad-image-label'>
+                                <h2>BIOCONTROL OF CROP PATHOGENS</h2>
+                            </div>
+
+                        </div>
+                        <div className = 'advantage-image-section' >
+                            <div className = 'ad-image-container'>
+                                <img src ='./advantage-images/web7-jpg.png' alt = 'hypergalaxy'/>
+
+                            </div>
+
+                            <div className = 'ad-image-label'>
+                                <h2>FACE CLIMATE CHANGES WITH BENEFICIALS</h2>
+                            </div>
+
+                        </div>
+                        <div className = 'advantage-image-section' >
+                            <div className = 'ad-image-container'>
+                                <img src ='./advantage-images/web8-jpg.png' alt = 'hypergalaxy'/>
+
+                            </div>
+
+                            <div className = 'ad-image-label'>
+                                <h2>MAINTAIN FIELD RESILENCE TO STRESS</h2>
+                            </div>
+
+                        </div>
+
+                        
+
+                    </div>
+            </section>
+
+
+            <section className = 'project-container'>
+                <div className = 'project-title-container'>
+                    <h1>Projects</h1>
 
                 </div>
 
-           
+                <div className = 'project-content-container'>
+
+                    <div className = 'project1-container'>
+                        <div className = 'project-image-container'>
+                            <img src = './project_images/Slide6-JPG.png' alt = 'mircobial growth'/>
+                           
+
+                        </div>
+
+                        <div className = "project-text-container">
+                            <h1>Building Soil Health</h1>
+                        </div>
+
+                    </div>
+
+                    <div className = 'project2-container'>
+
+
+                        <div className = "project-text-container">
+                            <h1>20 YEARS OF FIELD RESULTS WITH U.S. POTATO GROWERS</h1>
+                        </div>
+
+                        <div className = 'project-image-container'>
+                            <img src = './project_images/Slide18-JPG.png' alt = 'Potato Statistics'/>
+
+                        </div>
+
+                       
+
+                    </div>
+
+                    <div className = 'project3-container'>
+                        <div className = 'project-image-container'>
+                            <img src = './project_images/Slide28-JPG.png' alt = 'Tubers in Potatoes'/>
+
+                        </div>
+
+                        <div className = "project-text-container">
+                       
+                            <h1>EARLY TUBER INITIATION AND QUALITY</h1>
+                        </div>
+
+                    </div>
+
+                </div>
 
 
             </section>
 
+            
+           
+
+          
             <div className = 'footer-container'>
                 <Footer/>
 
